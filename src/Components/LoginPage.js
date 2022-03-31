4c624160f7ff0c9339730a3bf1704b321a5c46e5
Pawel Durczak <kissonion24@gmail.com>
2022-03-31 22:47:13 +0200

    Added logging login/logout events

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -6,6 +6,7 @@ import {
   signInWithPopup,
   onAuthStateChanged,
 } from "firebase/auth";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 function LoginPage(props) {
   const provider = new GoogleAuthProvider();
@@ -21,35 +22,33 @@ function LoginPage(props) {
 
   const Login = () => {
     const auth = getAuth();
+    const analytics = getAnalytics();
+
     signInWithPopup(auth, provider)
       .then((result) => {
-        // // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-        // // The signed-in user info.
-        // const user = result.user;
-        // // ...
         alert("Pomyślnie zalogowano");
+        logEvent(analytics, "login_succesfull", {
+          user_name: result.user,
+        });
         setLoggedIn(true);
         setUserName(getAuth().currentUser);
       })
       .catch((error) => {
         alert(`Wystąpił błąd przy logowaniu: ${error.message}`);
+        logEvent(analytics, "login_failed", {
+          user_name: error.email,
+        });
         setLoggedIn(false);
         setUserName(getAuth().currentUser);
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // // The email of the user's account used.
-        // const email = error.email;
-        // // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
-        // // ...
       });
   };
 
   const Logout = () => {
     const auth = getAuth();
+    const analytics = getAnalytics();
+    logEvent(analytics, "logout", {
+      user_name: auth.currentUser,
+    });
     auth.signOut();
     setLoggedIn(null);
   };
